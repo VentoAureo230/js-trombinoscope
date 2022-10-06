4b1839f6128c0c428d21e556b8fa71b59a83ebbf
VentoAureo230 <anthony.meny35@gmail.com>
2022-10-06 21:50:17 +0200

    IMG UPDATE

--- trombi.js
+++ trombi.js
@@ -1,3 +1,51 @@
-fetch("https://github.com/VentoAureo230")
-	.then(data => data.json)
-	.then(console.log)
+async function getFile() {
+	let students = [];
+	await fetch("/etudiants.json")
+		.then((data) => data.json())
+		.then((data) => { students = data.students})
+	return students		
+}
+
+function createRow(data){
+
+	const loginDiv = document.createElement("p")
+	loginDiv.innerHTML = "Login : " + data.login
+	document.body.appendChild(loginDiv)
+
+	const nameDiv = document.createElement("p")
+	nameDiv.innerHTML = "Nom : " + data.name
+	document.body.appendChild(nameDiv)
+	
+
+	const bioDiv = document.createElement("p")
+	bioDiv.innerHTML = "Biographie : " + data.bio
+	document.body.appendChild(bioDiv)
+ 
+	const followersDiv = document.createElement("p")
+	followersDiv.innerHTML = "Followers : " + data.followers
+	document.body.appendChild(followersDiv)
+
+	const locationDiv = document.createElement("p")
+	locationDiv.innerHTML = "Location : " + data.location
+	document.body.appendChild(locationDiv)
+
+	const avatarDiv =  document.createElement("img")
+	avatarDiv.src = data.avatar_url
+	document.body.appendChild(avatarDiv)
+}
+
+async function search() {
+	let logins = await getFile()
+	for(login of logins)
+		if (login != null) {
+			fetch(`https://api.github.com/users/${login}`, {
+				headers: new Headers({"Authorization": "Bearer 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"})
+			})
+			.then((data) => data.json())
+			.then((data) => createRow(data))
+			
+		} else {
+			console.error('Erreur pendant le chargement');
+		}
+}
+
